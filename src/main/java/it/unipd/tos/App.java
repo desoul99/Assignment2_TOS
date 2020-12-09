315a0c8456cf91737fddf84635f4c08f30d2c415
Marco Dello Iacovo <marco.delloiacovo99@gmail.com>
2020-12-09 09:24:16 +0100

    Generato progetto con archetype Maven

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Marco] [Dello Iacovo] [1193421]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
